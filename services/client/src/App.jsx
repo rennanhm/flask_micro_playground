e9d4183061e11a25754c18a6235f0318d6fff73a
Rennan Almeida <rennanhm@gmail.com>
2019-10-03 14:16:02 -0300

    inicio implementação autenticação

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,6 +7,7 @@ import AddUser from './components/AddUser'
 import About from './components/About'
 import NavBar from './components/NavBar'
 import Form from './components/Form'
+import Logout from './components/Logout'
 
 
 class App extends Component {
@@ -21,10 +22,14 @@ class App extends Component {
                 username: '',
                 email: '',
                 password: ''
-            }
+            },
+            isAuthenticated: false,
         };
         this.addUser = this.addUser.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+        this.handleFormChange = this.handleFormChange.bind(this);
+        this.logoutUser = this.logoutUser.bind(this);
     };
 
     componentDidMount() {
@@ -63,10 +68,52 @@ class App extends Component {
         this.setState(obj);
     };
 
+    handleFormChange(event) {
+        const obj = this.state.formData;
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    };
+
+    handleUserFormSubmit(event) {
+        event.preventDefault();
+        const formType = window.location.href.split('/').reverse()[0]
+        let data = {
+            email: this.state.formData.email,
+            password: this.state.formData.password
+        };
+        if (formType === 'register') {
+            data.username = this.state.formData.username;
+        }
+        const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`;
+        axios.post(url, data)
+            .then((res) => {
+                this.clearFormState();
+                window.localStorage.setItem('authToken', res.data.auth_token);
+                this.setState({isAuthenticated: true})
+                this.getUsers();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    clearFormState() {
+        this.setState({
+            formData: {username: '', email: '', password: ''},
+            username: '',
+            email: ''
+        })
+    }
+
+    logoutUser() {
+        window.localStorage.clear();
+        this.setState({ isAuthenticated: false });
+    }
+
     render() {
         return (
             <div>
-                <NavBar title={this.state.title} />
+                <NavBar title={this.state.title}/>
                 <section className="section">
                     <div className="container">
                         <div className="columns">
@@ -77,6 +124,9 @@ class App extends Component {
                                         <Form
                                             formType={'Register'}
                                             formData={this.state.formData}
+                                            handleUserFormSubmit={this.handleUserFormSubmit}
+                                            handleFormChange={this.handleFormChange}
+                                            isAuthenticated={this.state.isAuthenticated}
                                         />
                                     )}
                                     />
@@ -84,12 +134,16 @@ class App extends Component {
                                         <Form
                                             formType={'Login'}
                                             formData={this.state.formData}
+                                            handleUserFormSubmit={this.handleUserFormSubmit}
+                                            handleFormChange={this.handleFormChange}
+                                            isAuthenticated={this.state.isAuthenticated}
                                         />
-                                    )} />
+                                    )}/>
                                     <Route exact path='/' render={() => (
                                         <div>
                                             <h1 className="title is-1">All Users</h1>
-                                            <hr/><br/>
+                                            <hr/>
+                                            <br/>
                                             <AddUser
                                                 username={this.state.username}
                                                 email={this.state.email}
@@ -99,8 +153,14 @@ class App extends Component {
                                             <br/><br/>
                                             <UsersList users={this.state.users}/>
                                         </div>
-                                    )} />
+                                    )}/>
                                     <Route exact path='/about' component={About}/>
+                                    <Route exact path='/logout' render={() => (
+                                        <Logout
+                                            logoutUser={this.logoutUser}
+                                            isAuthenticated={this.state.isAuthenticated}
+                                        />
+                                    )}/>
                                 </Switch>
                             </div>
                         </div>
