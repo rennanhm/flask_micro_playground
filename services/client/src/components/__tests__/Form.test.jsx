6f0d5e67288e858373d80cdc1ba1a0090577fb33
Rennan Almeida <rennanhm@gmail.com>
2019-10-06 16:20:28 -0300

    Client: refatorando form component

--- services/client/src/components/__tests__/Form.test.jsx
+++ services/client/src/components/__tests__/Form.test.jsx
@@ -14,8 +14,7 @@ const testData = [
             email: '',
             password: ''
         },
-        handleUserFormSubmit: jest.fn(),
-        handleFormChange: jest.fn(),
+        loginUser: jest.fn(),
         isAuthenticated: false,
     },
     {
@@ -25,11 +24,10 @@ const testData = [
             email: '',
             password: ''
         },
-        handleUserFormSubmit: jest.fn(),
-        handleFormChange: jest.fn(),
+        loginUser: jest.fn(),
         isAuthenticated: false,
     }
-]
+];
 
 describe('When not authenticated', () => {
     testData.forEach((el) => {
@@ -47,14 +45,14 @@ describe('When not authenticated', () => {
         });
         it(`${el.formType} Form submits the form properly`, () => {
             const wrapper = shallow(component);
+            wrapper.instance().handleUserFormSubmit = jest.fn();
+            wrapper.update();
             const input = wrapper.find('input[type="email"]');
-            expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(0);
-            expect(el.handleFormChange).toHaveBeenCalledTimes(0);
-            input.simulate('change')
-            expect(el.handleFormChange).toHaveBeenCalledTimes(1);
+            expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(0);
+            input.simulate('change', { target: { name: 'email', value: 'test@test.com'} });
             wrapper.find('form').simulate('submit', el.formData);
-            expect(el.handleUserFormSubmit).toHaveBeenCalledWith(el.formData);
-            expect(el.handleUserFormSubmit).toHaveBeenCalledTimes(1);
+            expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledWith(el.formData);
+            expect(wrapper.instance().handleUserFormSubmit).toHaveBeenCalledTimes(1);
         });
         it(`${el.formType} Form renders a snapshot properly`, () => {
             const tree = renderer.create(component).toJSON();
